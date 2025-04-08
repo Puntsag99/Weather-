51ef1b12e21ad26a746976b464ba145064628ab0
unknown <puntsagg0@gmail.com>
2025-04-09 07:16:46 +0800

    gffd

--- src/components/day/Input.jsx
+++ src/components/day/Input.jsx
@@ -6,11 +6,15 @@ import { useEffect, useState } from "react";
 export const Input = () => {
   const [countries, setCountries] = useState([]);
   const [userInput, setUserInput] = useState("");
-  console.log("ene bol state country:", countries);
-
-  const handleChanhe = (event) => {
-    event.target.value;
+  // console.log("ene bol state country:", countries);
+  // console.log("ene bol user utag:", userInput);
+  const handleChange = (event) => {
+    setUserInput(event.target.value);
   };
+
+  const filteredCountries = countries.filter((item) =>
+    item.country.toLowerCase().startsWith(userInput.toLowerCase())
+  );
 
   useEffect(() => {
     async function fetchcountries() {
@@ -19,7 +23,7 @@ export const Input = () => {
           "https://countriesnow.space/api/v0.1/countries"
         );
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
         setCountries(data.data);
       } catch (error) {
         console.error("aldaa :", error);
@@ -31,10 +35,23 @@ export const Input = () => {
     <div className=" flex w-[567px] py-4 px-6 bg-white gap-x-4 rounded-[48px]">
       <Search className="w-[48px] h-[48px] opacity-20" />
       <input
+        value={userInput}
+        onChange={handleChange}
         type="text"
         placeholder="Search"
         className="font-bold text-[32px] w-full  outline-none"
       />
+
+      {/* {filteredCountries.map((item, index) => (
+        <div key={`${item.county}-${index}`} className="mb-4">
+          <h2 className="text-lg font-bold">{item.country}</h2>
+          <ul className="list-disc list-inside">
+            {item.cities.map((city, index) => (
+              <li key={`${city}-${index}`}>{city}</li>
+            ))}
+          </ul>
+        </div>
+      ))} */}
     </div>
   );
 };
